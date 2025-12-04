94e1e03fa1f3cfd3182b6266d06bb08795f9825c
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2025-12-04 13:34:36 -0300

    Implemented jump animation on prototype

--- Assets/Player/Player.cs
+++ Assets/Player/Player.cs
@@ -1,15 +1,24 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class Player : MonoBehaviour
 {
 
     public Rigidbody2D myRigidBody;
+
+    [Header("Speed Setup")]
     public Vector2 friction = new Vector2(.1f, 0);
     public float speed = 5f;
     public float speedRun = 10f;
     public float jumpForce = 10f;
+
+    [Header("Animation Setup")]
+    public float jumpScaleY = 1.5f;
+    public float jumpScaleX = .5f;
+    public float duration = .3f;
+    public Ease ease = Ease.OutBack;
 
     private float _currentSpeed;
     private void Update()
@@ -52,6 +61,17 @@ public class Player : MonoBehaviour
         if (Input.GetKeyDown(KeyCode.Space))
         {
             myRigidBody.velocity = Vector2.up * jumpForce;
+            myRigidBody.transform.localScale = Vector2.one;
+
+            DOTween.Kill(myRigidBody.transform);
+
+            HandleScaleJump();
         }
+    }
+
+    private void HandleScaleJump()
+    {
+        myRigidBody.transform.DOScaleY(jumpScaleY, duration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
+        myRigidBody.transform.DOScaleX(jumpScaleX, duration).SetLoops(2, LoopType.Yoyo).SetEase(ease);
     }
 }
