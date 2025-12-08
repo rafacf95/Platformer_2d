c7eb89e7f0d2c303e47c90d151e3ec91e4bc9542
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2025-12-08 13:56:20 -0300

    Added animation for PFB_Coin

--- Assets/Scripts/Collectable/ItemCollectableBase.cs
+++ Assets/Scripts/Collectable/ItemCollectableBase.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class ItemCollectableBase : MonoBehaviour
 {
+    [Header("Configuration")]
     public string compareTag = "Player";
 
     private void OnTriggerEnter2D(Collider2D collision)
--- Assets/Scripts/Collectable/ItemCollectableCoin.cs
+++ Assets/Scripts/Collectable/ItemCollectableCoin.cs
@@ -1,12 +1,16 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using DG.Tweening;
 
 public class ItemCollectableCoin : ItemCollectableBase
 {
+
     protected override void OnCollect()
     {
         base.OnCollect();
         ItemManager.Instance.AddCoin();
     }
+
+
 }
