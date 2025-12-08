bbdf26c0aa01a6a8aea871a214bb59273fcef7b8
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2025-12-08 16:42:04 -0300

    Added animation for Enemy and player deaths

--- Assets/Scripts/Enemy/EnemyBase.cs
+++ Assets/Scripts/Enemy/EnemyBase.cs
@@ -2,12 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class EnemyBase : MonoBehaviour
+public class EnemyBase : HealthBase
 {
+    [Header("Enemy setup")]
     public int damage = 10;
 
     public Animator animator;
     public string triggerAttack = "Attack";
+    public string triggerDeath = "Death";
 
     // private void OnTriggerEnter2D(Collider2D collision)
     // {
@@ -36,6 +38,22 @@ public class EnemyBase : MonoBehaviour
     private void PlayAttackAnimation()
     {
         animator.SetTrigger(triggerAttack);
+    }
+
+    private void PlayDeathAnimation()
+    {
+        animator.SetTrigger(triggerDeath);
+    }
+
+    private void OnEnemyKill()
+    {
+        OnKill -= OnEnemyKill;
+        PlayDeathAnimation();
+    }
+
+    void Start()
+    {
+        OnKill += OnEnemyKill;
     }
 
 }
