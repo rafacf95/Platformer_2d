b49323a91609481100c2a4dfa70542f5929a6ca6
Rafael Camargo <70503254+rafacf95@users.noreply.github.com>
2025-12-09 14:26:56 -0300

    Created new collectable

--- Assets/Scripts/Collectable/ItemManager.cs
+++ Assets/Scripts/Collectable/ItemManager.cs
@@ -7,20 +7,25 @@ using TMPro;
 public class ItemManager : Singleton<ItemManager>
 {
     public SOInt coins;
+    public SOInt lifes;
     public TextMeshProUGUI coinText;
+    public TextMeshProUGUI lifeText;
 
     private void Reset()
     {
         coins.value = 0;
-        // coinText.text = "x " + ItemManager.Instance.coins.value.ToString();
+        lifes.value = 0;
     }
 
     public void AddCoin(int amount = 1)
     {
         coins.value += amount;
-        // coinText.text = "x " + ItemManager.Instance.coins.value.ToString();
     }
 
+    public void AddLife(int amount = 1)
+    {
+        lifes.value += amount;
+    }
     void Start()
     {
         Reset();
